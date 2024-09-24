c32f654400da66f3e3f70ec421526bb428afc5ab
Enes Karakuş <mail.eneskarakus@gmail.com>
2024-09-24 22:06:30 +0300

    Update Transaction Server Side

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use App\Models\ItemTransaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view("modules.dashboard.index.index");
+    public function index()
+    {
+        $userCount = User::count();
+        $itemCount = Item::count();
+        $mostUsedServer = Item::select('servers.name')
+            ->leftJoin('servers', 'items.server_id', '=', 'servers.id') // LEFT JOIN
+            ->whereNull('items.deleted_at')                             // WHERE items.deleted_at IS NULL
+            ->groupBy('servers.name')                                   // GROUP BY servers.name
+            ->orderByRaw('COUNT(*) DESC')                               // ORDER BY COUNT(*) DESC
+            ->limit(1)                                                  // LIMIT 1
+            ->first();
+        $bestTransactionItem = ItemTransaction::with('item')
+            ->select('item_id')
+            ->selectRaw('COUNT(*) as transaction_count')
+            ->groupBy('item_id')
+            ->orderBy('transaction_count', 'desc')
+            ->first();
+
+
+        return view("modules.dashboard.index.index", compact('userCount', 'itemCount', 'bestTransactionItem', 'mostUsedServer'));
     }
 }
--- resources/views/modules/dashboard/index/index.blade.php
+++ resources/views/modules/dashboard/index/index.blade.php
@@ -3,22 +3,75 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-12">
-            @if (session()->has('success'))
-                <div class="alert alert-success">
-                    {{ session()->get('success') }}
-                </div>
-            @endif
+        <div class="col-lg-3 col-xs-6">
 
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title pull-left">Dashboard</h3>
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>{{$itemCount}}</h3>
+                    <p>Item Adeti</p>
                 </div>
-                <div class="box-body with-border">
-                    {{--                    <canvas id="itemChart"--}}
-                    {{--                            width="400"--}}
-                    {{--                            height="150"></canvas>--}}
+                <div class="icon">
+                    <i class="fa fa-shopping-cart"></i>
                 </div>
+                <a href="#"
+                   class="small-box-footer">
+                    More info
+                    <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>{{$mostUsedServer->name}}<sup style="font-size: 20px"></sup></h3>
+                    <p>En çok itemin bulunduğu server</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#"
+                   class="small-box-footer">
+                    More info
+                    <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>{{$userCount}}</h3>
+                    <p>Sistemde tanımlı kullanıcı adeti</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#"
+                   class="small-box-footer">
+                    More info
+                    <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>{{$bestTransactionItem->item->name}}</h3>
+                    <p>En popüler item</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#"
+                   class="small-box-footer">
+                    More info
+                    <i class="fa fa-arrow-circle-right"></i>
+                </a>
             </div>
         </div>
 
